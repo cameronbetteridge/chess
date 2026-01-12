103643928ac6a8d1c81932d114c909675727c8ac
Cameron Betteridge <cameron@Camerons-MacBook-Pro.local>
2026-01-12 14:03:48 -0700

    Implemented ChessBoard.resetBoard
    ChessBoard constructor calls resetBoard\n- Created initializeBackRow and initializePawnsRow helper methods for resetBoard\n- resetBoard and is helper methods utilize the as-yet-unimplemented addPiece method

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -8,8 +8,10 @@ package chess;
  */
 public class ChessBoard {
 
+    ChessPiece[][] boardPieces = new ChessPiece[8][8];
+
     public ChessBoard() {
-        
+        resetBoard();
     }
 
     /**
@@ -33,11 +35,41 @@ public class ChessBoard {
         throw new RuntimeException("Not implemented");
     }
 
+    private void initializeBackRow(int row, ChessGame.TeamColor color) {
+        row--;
+
+        addPiece(new ChessPosition(row, 0), new ChessPiece(color, ChessPiece.PieceType.ROOK));
+        addPiece(new ChessPosition(row, 1), new ChessPiece(color, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(row, 2), new ChessPiece(color, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(row, 3), new ChessPiece(color, ChessPiece.PieceType.QUEEN));
+        addPiece(new ChessPosition(row, 4), new ChessPiece(color, ChessPiece.PieceType.KING));
+        addPiece(new ChessPosition(row, 5), new ChessPiece(color, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(row, 6), new ChessPiece(color, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(row, 7), new ChessPiece(color, ChessPiece.PieceType.ROOK));
+    }
+
+    private void initializePawnsRow(int row, ChessGame.TeamColor color) {
+        row--;
+
+        for (int i = 0; i < 8; i++) {
+            addPiece(new ChessPosition(row, i), new ChessPiece(color, ChessPiece.PieceType.PAWN));
+        }
+    }
+
     /**
      * Sets the board to the default starting board
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        initializeBackRow(1, ChessGame.TeamColor.WHITE);
+        initializeBackRow(8, ChessGame.TeamColor.BLACK);
+        initializePawnsRow(2, ChessGame.TeamColor.WHITE);
+        initializePawnsRow(7, ChessGame.TeamColor.BLACK);
+
+        for (int row = 3; row < 7; row++) {
+            for (int col = 1; col < 9; col++) {
+                addPiece(new ChessPosition(row, col), null);
+            }
+        }
     }
 }
