5dcfb158ed1f2e5f72052a6eae10fc12388a091a
Cameron Betteridge <cameron@Camerons-MacBook-Pro.local>
2026-02-25 11:50:13 -0700

    Implemented MemoryUserDAO.java

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,4 +1,32 @@
 package dataaccess;
 
+import model.UserData;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MemoryUserDAO implements UserDAO {
+    private Map<String, UserData> users;
+
+    public MemoryUserDAO() {
+        users = new HashMap<>();
+    }
+
+    public void createUser(UserData user) throws DataAccessException {
+        if (users.containsKey(user.username())) {
+            throw new DataAccessException("Error: already taken");
+        }
+        users.put(user.username(), user);
+    }
+
+    public UserData getUser(String username) throws DataAccessException {
+        if (!users.containsKey(username)) {
+            throw new DataAccessException("Error: does not exist");
+        }
+        return users.get(username);
+    }
+
+    public void clear() {
+        users = new HashMap<>();
+    }
 }
