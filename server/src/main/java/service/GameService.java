62001ac91d50e51e647d5107edc9de42ef3da566
Cameron Betteridge <cameron@Camerons-MacBook-Pro.local>
2026-02-25 16:39:03 -0700

    Implemented GameService constructor

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,15 +1,32 @@
 package service;
 
+import dataaccess.AuthDAO;
 import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
 
 public class GameService {
+    private final UserDAO userDAO;
+    private final GameDAO gameDAO;
+    private final AuthDAO authDAO;
+
+    public GameService(UserDAO userDAO, GameDAO gameDAO, AuthDAO authDAO) {
+        this.userDAO = userDAO;
+        this.gameDAO = gameDAO;
+        this.authDAO = authDAO;
+    }
+
     public ListResult list(ListRequest listRequest) throws DataAccessException {
 
     }
+
     public CreateGameResult createGame(CreateGameRequest createGameRequest) throws DataAccessException {
 
     }
+
     public void joinGame(JoinGameRequest joinGameRequest) throws DataAccessException {
 
     }
+
+    public void clear() {}
 }
