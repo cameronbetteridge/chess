4dfff2475f532628570f55d22ea4cf6f7fdbe40a
Cameron Betteridge <cameron@Camerons-MacBook-Pro.local>
2026-02-25 17:15:37 -0700

    Implemented GameService.createGame

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,9 +1,15 @@
 package service;
 
+import chess.ChessGame;
 import dataaccess.AuthDAO;
 import dataaccess.DataAccessException;
 import dataaccess.GameDAO;
 import dataaccess.UserDAO;
+import model.GameData;
+
+import javax.xml.crypto.Data;
+import java.util.Collection;
+import java.util.Random;
 
 public class GameService {
     private final UserDAO userDAO;
@@ -16,17 +22,44 @@ public class GameService {
         this.authDAO = authDAO;
     }
 
-    public ListResult list(ListRequest listRequest) throws DataAccessException {
+//    public ListResult list(ListRequest request) throws DataAccessException {
+//
+//    }
+
+    public CreateGameResult createGame(CreateGameRequest request) throws DataAccessException {
+        authDAO.getAuth(request.authToken());
+        if (request.gameName().isEmpty()) {
+            throw new DataAccessException("Error: bad request");
+        }
+        int gameID = createGameID();
+        GameData game = new GameData(gameID, null, null, request.gameName(), new ChessGame());
+        gameDAO.createGame(game);
+        return new CreateGameResult(gameID);
+    }
+
+    public void joinGame(JoinGameRequest request) throws DataAccessException {
 
     }
 
-    public CreateGameResult createGame(CreateGameRequest createGameRequest) throws DataAccessException {
+    public void clear() {
 
     }
 
-    public void joinGame(JoinGameRequest joinGameRequest) throws DataAccessException {
-
+    private int createGameID() {
+        int id;
+        boolean unique;
+        Random random = new Random();
+        Collection<GameData> games = gameDAO.listGames();
+        do {
+            id = Math.abs(random.nextInt());
+            unique = true;
+            for (GameData game : games) {
+                if (game.gameID() == id) {
+                    unique = false;
+                    break;
+                }
+            }
+        } while (!unique);
+        return id;
     }
-
-    public void clear() {}
 }
