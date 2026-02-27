307db367f2be7174ea0ef04f58be3da4a83922e3
Cameron Betteridge <cameron@Camerons-MacBook-Pro.local>
2026-02-27 15:04:26 -0700

    Implemented GameHandler and UserHandler constructors

--- server/src/main/java/server/GameHandler.java
+++ server/src/main/java/server/GameHandler.java
@@ -1,4 +1,11 @@
 package server;
 
+import service.GameService;
+
 public class GameHandler {
+    private final GameService gameService;
+
+    public GameHandler(GameService gameService) {
+        this.gameService = gameService;
+    }
 }
