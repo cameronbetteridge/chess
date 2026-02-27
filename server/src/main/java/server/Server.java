266f9d84e9bb832606b04ffe903080ed420c2b67
Cameron Betteridge <cameron@Camerons-MacBook-Pro.local>
2026-02-27 16:36:14 -0700

    Added endpoints to Server.java and implemented Server.exceptionHandler

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,16 +1,32 @@
 package server;
 
+import dataaccess.*;
 import io.javalin.*;
+import io.javalin.http.Context;
+import service.GameService;
+import service.UserService;
 
 public class Server {
-
     private final Javalin javalin;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        UserDAO userDAO = new MemoryUserDAO();
+        GameDAO gameDAO = new MemoryGameDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
+        UserService userService = new UserService(userDAO, authDAO);
+        GameService gameService = new GameService(userDAO, gameDAO, authDAO);
+        UserHandler userHandler = new UserHandler(userService);
+        GameHandler gameHandler = new GameHandler(gameService);
 
-        // Register your endpoints and exception handlers here.
-
+        javalin = Javalin.create(config -> config.staticFiles.add("web"))
+                .delete("/db", gameHandler::clear)
+                .post("/user", userHandler::register)
+                .post("/session", userHandler::login)
+                .delete("/session", userHandler::logout)
+                .get("/game", gameHandler::list)
+                .post("/game", gameHandler::createGame)
+                .put("/game", gameHandler::joinGame)
+                .exception(this::exceptionHandler);
     }
 
     public int run(int desiredPort) {
@@ -20,5 +36,10 @@ public class Server {
 
     public void stop() {
         javalin.stop();
+    }
+
+    private void exceptionHandler(DataAccessException e, Context context) {
+        context.status(e.toHttpStatusCode());
+        context.result(e.toJson());
     }
 }
