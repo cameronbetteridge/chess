06af8d70b7f8af228ac69f29247ba88f7d4c88e7
Cameron Betteridge <cameron@Camerons-MacBook-Pro.local>
2026-02-27 17:16:42 -0700

    Added GameHandler and UserHandler method signatures

--- server/src/main/java/server/GameHandler.java
+++ server/src/main/java/server/GameHandler.java
@@ -1,11 +1,28 @@
 package server;
 
 import service.GameService;
+import io.javalin.http.Context;
 
 public class GameHandler {
     private final GameService gameService;
 
     public GameHandler(GameService gameService) {
         this.gameService = gameService;
+    }
+
+    public void createGame(Context context) {
+
+    }
+
+    public void joinGame(Context context) {
+
+    }
+
+    public void list(Context context) {
+
+    }
+
+    public void clear(Context context) {
+
     }
 }
--- server/src/main/java/server/UserHandler.java
+++ server/src/main/java/server/UserHandler.java
@@ -1,11 +1,24 @@
 package server;
 
 import service.UserService;
+import io.javalin.http.Context;
 
 public class UserHandler {
     private final UserService userService;
 
     public UserHandler(UserService userService) {
         this.userService = userService;
+    }
+
+    public void register(Context context) {
+
+    }
+
+    public void login(Context context) {
+
+    }
+
+    public void logout(Context context) {
+
     }
 }
