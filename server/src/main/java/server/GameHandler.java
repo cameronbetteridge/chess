7b5d428dc9f16d3e70b32b5afeb614fab4f567e6
Cameron Betteridge <cameron@Camerons-MacBook-Pro.local>
2026-02-27 17:22:47 -0700

    Minor changes to signatures and implementation of UserHandler and GameHandler
    Methods now throw DataAccessExceptions

--- server/src/main/java/server/GameHandler.java
+++ server/src/main/java/server/GameHandler.java
@@ -1,24 +1,25 @@
 package server;
 
+import dataaccess.DataAccessException;
 import service.GameService;
 import io.javalin.http.Context;
 
 public class GameHandler {
-    private final GameService gameService;
+    private final GameService service;
 
-    public GameHandler(GameService gameService) {
-        this.gameService = gameService;
+    public GameHandler(GameService service) {
+        this.service = service;
     }
 
-    public void createGame(Context context) {
-
-    }
-
-    public void joinGame(Context context) {
+    public void createGame(Context context) throws DataAccessException {
 
     }
 
-    public void list(Context context) {
+    public void joinGame(Context context) throws DataAccessException {
+
+    }
+
+    public void list(Context context) throws DataAccessException {
 
     }
 
--- server/src/main/java/server/UserHandler.java
+++ server/src/main/java/server/UserHandler.java
@@ -1,24 +1,28 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import service.RegisterRequest;
+import service.RegisterResult;
 import service.UserService;
 import io.javalin.http.Context;
 
 public class UserHandler {
-    private final UserService userService;
+    private final UserService service;
 
-    public UserHandler(UserService userService) {
-        this.userService = userService;
+    public UserHandler(UserService service) {
+        this.service = service;
     }
 
-    public void register(Context context) {
-
-    }
-
-    public void login(Context context) {
+    public void register(Context context) throws DataAccessException {
 
     }
 
-    public void logout(Context context) {
+    public void login(Context context) throws DataAccessException {
+
+    }
+
+    public void logout(Context context) throws DataAccessException {
 
     }
 }
