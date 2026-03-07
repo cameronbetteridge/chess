15f619a226297d94b6c9603b1d7ade15ae295459
Cameron Betteridge <cameron@Mac.lan>
2026-03-07 14:38:42 -0700

    Updated SQLUserTests.java to reflect password hashing

--- server/src/test/java/dataaccess/SQLUserTests.java
+++ server/src/test/java/dataaccess/SQLUserTests.java
@@ -22,8 +22,7 @@ public class SQLUserTests {
     public void createUserPositiveTest() throws DataAccessException {
         UserData user = new UserData("user1", "123abc", "hi@test.com");
         userDAO.createUser(user);
-        UserData result = userDAO.getUser("user1");
-        Assertions.assertEquals(user, result);
+        Assertions.assertTrue(userDAO.verifyUser("user1", "123abc"));
     }
 
     @Test
@@ -36,12 +35,24 @@ public class SQLUserTests {
     public void getUserPositiveTest() throws DataAccessException {
         UserData result = userDAO.getUser("test");
         UserData expected = new UserData("test", "wordpass", "hi@test.com");
-        Assertions.assertEquals(expected, result);
+        Assertions.assertEquals(result.username(), expected.username());
+        Assertions.assertEquals(result.email(), expected.email());
+        Assertions.assertTrue(userDAO.verifyUser(expected.username(), expected.password()));
     }
 
     @Test
     public void getUserNegativeTest() throws DataAccessException {
         Assertions.assertThrows(DataAccessException.class, () -> userDAO.getUser("hi"), "Error: doesn't exist");
+    }
+
+    @Test
+    public void verifyUserPositiveTest() throws DataAccessException {
+
+    }
+
+    @Test
+    public void verifyUserNegativeTest() throws DataAccessException {
+
     }
 
     @Test
