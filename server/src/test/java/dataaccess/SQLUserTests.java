fbaa28832e0b12afe623cd6ca701bc064269024b
Cameron Betteridge <cameron@Camerons-MacBook-Pro.local>
2026-03-06 19:44:59 -0700

    Created createUser tests

--- server/src/test/java/dataaccess/SQLUserTests.java
+++ server/src/test/java/dataaccess/SQLUserTests.java
@@ -1,4 +1,34 @@
 package dataaccess;
 
+import model.UserData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class SQLUserTests {
+    private UserDAO userDAO;
+
+    public SQLUserTests() throws DataAccessException {
+        userDAO = new MySQLUserDAO();
+    }
+
+    @BeforeEach
+    public void resetTests() throws DataAccessException {
+        userDAO.clear();
+        userDAO.createUser(new UserData("test", "wordpass", "hi@test.com"));
+    }
+
+    @Test
+    public void createUserPositiveTest() throws DataAccessException {
+        UserData user = new UserData("user1", "123abc", "hi@test.com");
+        userDAO.createUser(user);
+        UserData result = userDAO.getUser("user1");
+        Assertions.assertEquals(user, result);
+    }
+
+    @Test
+    public void createUserNegativeTest() throws DataAccessException {
+        UserData user = new UserData("test", "123abc", "hi@test.com");
+        Assertions.assertThrows(DataAccessException.class, () -> userDAO.createUser(user), "Error: already taken");
+    }
 }
