51993f502715814d3acc009d630333546d629b96
Cameron Betteridge <cameron@Camerons-MacBook-Pro.local>
2026-03-07 17:55:20 -0700

    Created createGame and getGame mysql tests

--- server/src/test/java/dataaccess/SQLGameTests.java
+++ server/src/test/java/dataaccess/SQLGameTests.java
@@ -4,6 +4,7 @@ import chess.ChessGame;
 import model.AuthData;
 import model.GameData;
 import model.UserData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,7 @@ public class SQLGameTests {
     private final AuthDAO authDAO;
     private final UserDAO userDAO;
     private final GameDAO gameDAO;
+    private int gameID;
 
     public SQLGameTests() throws DataAccessException {
         authDAO = new MySQLAuthDAO();
@@ -25,51 +27,58 @@ public class SQLGameTests {
         gameDAO.clear();
         userDAO.createUser(new UserData("test", "testPassword", "testEmail"));
         authDAO.createAuth(new AuthData("test", "test"));
-        gameDAO.createGame(new GameData(-1, null, null, "testGame", new ChessGame()));
+        gameID = gameDAO.createGame(new GameData(-1, null, null, "testGame", new ChessGame()));
     }
 
     @Test
-    public void createGamePositiveTest() {
-
+    public void createGamePositiveTest() throws DataAccessException {
+        GameData game = new GameData(-1, null, null, "game1", new ChessGame());
+        int gameID = gameDAO.createGame(game);
+        GameData result = gameDAO.getGame(gameID);
+        GameData expected = new GameData(gameID, null, null, "game1", new ChessGame());
+        Assertions.assertEquals(expected, result);
     }
 
     @Test
-    public void createGameNegativeTest() {
-
+    public void createGameNegativeTest() throws DataAccessException {
+        GameData game = new GameData(-1, "player1", null, "game1", new ChessGame());
+        Assertions.assertThrows(DataAccessException.class, () -> gameDAO.createGame(game));
     }
 
     @Test
-    public void getGamePositiveTest() {
-
+    public void getGamePositiveTest() throws DataAccessException {
+        GameData expected = new GameData(gameID, null, null, "testGame", new ChessGame());
+        GameData result = gameDAO.getGame(gameID);
+        Assertions.assertEquals(expected, result);
     }
 
     @Test
-    public void getGameNegativeTest() {
-
+    public void getGameNegativeTest() throws DataAccessException {
+        Assertions.assertThrows(DataAccessException.class, () -> gameDAO.getGame(3131));
     }
 
     @Test
-    public void listGamesPositiveTest() {
-
-    }
-
-    @Test
-    public void listGamesNegativeTest() {
-
-    }
-
-    @Test
-    public void updateGamePositiveTest() {
-
-    }
-
-    @Test
-    public void updateGameNegativeTest() {
+    public void listGamesPositiveTest() throws DataAccessException {
 
     }
 
     @Test
-    public void clearTest() {
+    public void listGamesNegativeTest() throws DataAccessException {
+
+    }
+
+    @Test
+    public void updateGamePositiveTest() throws DataAccessException {
+
+    }
+
+    @Test
+    public void updateGameNegativeTest() throws DataAccessException {
+
+    }
+
+    @Test
+    public void clearTest() throws DataAccessException {
 
     }
 }
