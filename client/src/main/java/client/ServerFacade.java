d6f4d9825d03140f2523d6c40f2b7072e7150037
Cameron Betteridge <cameron@Camerons-MacBook-Pro.local>
2026-03-21 17:52:56 -0600

    Created ServerFacade method signatures

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,4 +1,35 @@
 package client;
 
+import chess.ChessGame;
+
+import java.util.ArrayList;
+
 public class ServerFacade {
+    public String register(String username, String password, String email) {
+
+    }
+
+    public String login(String username, String password) {
+
+    }
+
+    public void logout(String authToken) {
+
+    }
+
+    public ArrayList<ChessGame> listGames(String authToken) {
+
+    }
+
+    public int createGame(String authToken, String gameName) {
+
+    }
+
+    public void joinGame(String authToken, int gameID, String playerColor) {
+
+    }
+
+    public void clear() {
+
+    }
 }
