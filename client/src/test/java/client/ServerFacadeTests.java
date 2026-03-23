ca099404d2b30fc7f7005a8d4bab8a12c61596d6
Cameron Betteridge <cameron@Camerons-MacBook-Pro.local>
2026-03-23 11:24:03 -0600

    Created ServerFacadeTests method signatures

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -5,14 +5,15 @@ import server.Server;
 
 
 public class ServerFacadeTests {
-
     private static Server server;
+    private static ServerFacade serverFacade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        serverFacade = new ServerFacade();
     }
 
     @AfterAll
@@ -20,10 +21,74 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @Test
+    public void registerPositiveTest() {
+
+    }
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void registerNegativeTest() {
+
+    }
+
+    @Test
+    public void loginPositiveTest() {
+
+    }
+
+    @Test
+    public void loginNegativeTest() {
+
+    }
+
+    @Test
+    public void logoutPositiveTest() {
+
+    }
+
+    @Test
+    public void logoutNegativeTest() {
+
+    }
+
+    @Test
+    public void listPositiveTest() {
+
+    }
+
+    @Test
+    public void listNegativeTest() {
+
+    }
+
+    @Test
+    public void createPositiveTest() {
+
+    }
+
+    @Test
+    public void createNegativeTest() {
+
+    }
+
+    @Test
+    public void joinPositiveTest() {
+
+    }
+
+    @Test
+    public void joinNegativeTest() {
+
+    }
+
+    @Test
+    public void clearPositiveTest() {
+
+    }
+
+    @Test
+    public void clearNegativeTest() {
+
     }
 
 }
