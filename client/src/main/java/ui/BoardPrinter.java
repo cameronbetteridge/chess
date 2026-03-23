c1e21a70d365b7a9d03af78172177daa396009e6
Cameron Betteridge <cameron@Camerons-MacBook-Pro.local>
2026-03-23 10:10:57 -0600

    Created BoardPrinter method signatures

--- client/src/main/java/ui/BoardPrinter.java
+++ client/src/main/java/ui/BoardPrinter.java
@@ -1,9 +1,35 @@
 package ui;
 
 import chess.ChessBoard;
+import chess.ChessPiece;
+import ui.EscapeSequences;
 
 public class BoardPrinter {
     public void printBoard(ChessBoard board) {
+
+    }
+
+    private void printColumnLabels() {
+
+    }
+
+    private void printRow(ChessPiece[] pieces, boolean startBlackSquare) {
+
+    }
+
+    private void printSquare(ChessPiece piece, boolean blackSquare) {
+
+    }
+
+    private void printPiece(ChessPiece piece) {
+
+    }
+
+    private void printRowLabel(int rowNum) {
+
+    }
+
+    private void finishLine() {
 
     }
 }
