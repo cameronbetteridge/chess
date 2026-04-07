5c52a7a8b92195d4d1e98cd4202c323167adbab4
Cameron Betteridge <cameron@Camerons-MacBook-Pro.local>
2026-04-06 20:13:43 -0600

    Implemented ConnectCommand.java

--- shared/src/main/java/websocket/commands/ConnectCommand.java
+++ shared/src/main/java/websocket/commands/ConnectCommand.java
@@ -1,4 +1,20 @@
 package websocket.commands;
 
-public class ConnectCommand {
+public class ConnectCommand extends UserGameCommand {
+    private final ConnectType connectType;
+
+    public ConnectCommand(String authToken, int gameID, ConnectType connectType) {
+        super(CommandType.RESIGN, authToken, gameID);
+        this.connectType = connectType;
+    }
+
+    public ConnectType getConnectType() {
+        return connectType;
+    }
+
+    public enum ConnectType {
+        WHITE_PLAYER,
+        BLACK_PLAYER,
+        OBSERVER
+    }
 }
