7f9949d75f42e6573194b479c6b14dab061fb88b
Cameron Betteridge <cameron@Camerons-MacBook-Pro.local>
2026-04-06 20:15:46 -0600

    Implemented MakeMoveCommand.java

--- shared/src/main/java/websocket/commands/MakeMoveCommand.java
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -1,4 +1,16 @@
 package websocket.commands;
 
-public class MakeMoveCommand {
+import chess.ChessMove;
+
+public class MakeMoveCommand extends UserGameCommand {
+    private final ChessMove move;
+
+    public MakeMoveCommand(String authToken, int gameID, ChessMove move) {
+        super(CommandType.RESIGN, authToken, gameID);
+        this.move = move;
+    }
+
+    public ChessMove getMove() {
+        return move;
+    }
 }
