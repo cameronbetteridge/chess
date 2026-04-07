88bae090696c9dd691aed9fa3cdf02ff3de81bc4
Cameron Betteridge <cameron@Camerons-MacBook-Pro.local>
2026-04-06 19:58:10 -0600

    Implemented WebSocketHandler.handleMessage

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,4 +1,77 @@
 package server.websocket;
 
-public class WebSocketHandler {
+import com.google.gson.Gson;
+import io.javalin.websocket.WsCloseContext;
+import io.javalin.websocket.WsCloseHandler;
+import io.javalin.websocket.WsConnectContext;
+import io.javalin.websocket.WsConnectHandler;
+import io.javalin.websocket.WsMessageContext;
+import io.javalin.websocket.WsMessageHandler;
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+
+public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
+
+    private final ConnectionManager connections = new ConnectionManager();
+
+    @Override
+    public void handleConnect(WsConnectContext ctx) {
+        System.out.println("Websocket connected");
+        ctx.enableAutomaticPings();
+    }
+
+    @Override
+    public void handleMessage(WsMessageContext ctx) {
+        try {
+            UserGameCommand command = new Gson().fromJson(ctx.message(), UserGameCommand.class);
+            switch (command.getCommandType()) {
+                case CONNECT -> connect(command.getAuthToken(), command.getGameID(), ctx.session);
+                case MAKE_MOVE -> makeMove(command.getAuthToken(), command.getGameID(), ctx.session);
+                case RESIGN -> resign(command.getAuthToken(), command.getGameID(), ctx.session);
+                case LEAVE -> leave(command.getAuthToken(), command.getGameID(), ctx.session);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void handleClose(WsCloseContext ctx) {
+        System.out.println("Websocket closed");
+    }
+
+    private void connect(String authToken, int gameID, Session session) throws IOException {
+        connections.add(session);
+        var message = String.format("%s is in the shop", visitorName);
+        var notification = new ServerMessage(ServerMessage.ServerMessageType.ARRIVAL, message);
+        connections.broadcast(session, gameID, notification);
+    }
+
+    private void makeMove(String authToken, int gameID, Session session) throws IOException {
+
+    }
+
+    private void resign(String authToken, int gameID, Session session) throws IOException {
+
+    }
+
+    private void leave(String authToken, int gameID, Session session) throws IOException {
+        var message = String.format("%s left the shop", visitorName);
+        var notification = new ServerMessage(ServerMessage.ServerMessageType.DEPARTURE, message);
+        connections.broadcast(session, notification);
+        connections.remove(session);
+    }
+
+    public void makeNoise(String petName, String sound) throws ResponseException {
+        try {
+            var message = String.format("%s says %s", petName, sound);
+            var notification = new ServerMessage(ServerMessage.ServerMessageType.NOISE, message);
+            connections.broadcast(null, notification);
+        } catch (Exception ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+    }
 }
