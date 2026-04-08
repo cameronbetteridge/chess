e1171ff9a77171b2656dafca4e4ca7154f753530
Cameron Betteridge <cameron@Camerons-MacBook-Pro.local>
2026-04-07 18:49:21 -0600

    Created WebsocketCommunicator class

--- client/src/main/java/client/WebsocketCommunicator.java
+++ client/src/main/java/client/WebsocketCommunicator.java
@@ -1,17 +1,61 @@
 package client;
 
-import java.net.http.HttpClient;
+import com.google.gson.Gson;
 
-public class WebsocketCommunicator {
-    private final String serverUrl;
-    private final HttpClient httpClient;
+import jakarta.websocket.*;
 
-    public WebsocketCommunicator(String host, int port) {
-        serverUrl = "http://" + host + ":" + port;
-        httpClient = HttpClient.newHttpClient();
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class WebsocketCommunicator extends Endpoint {
+
+    Session session;
+    ServerMessageHandler serverMessageHandler;
+
+    public WebSocketFacade(String url, ServerMessageHandler ServerMessageHandler) throws ResponseException {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/ws");
+            this.serverMessageHandler = ServerMessageHandler;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            //set message handler
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    Notification notification = new Gson().fromJson(message, Notification.class);
+                    ServerMessageHandler.notify(notification);
+                }
+            });
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
     }
 
-    public void connectWebsocket(String authToken, int gameID) throws Exception {
-
+    //Endpoint requires this method, but you don't have to do anything
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
     }
+
+    public void enterPetShop(String visitorName) throws ResponseException {
+        try {
+            var action = new Action(Action.Type.ENTER, visitorName);
+            this.session.getBasicRemote().sendText(new Gson().toJson(action));
+        } catch (IOException ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+    }
+
+    public void leavePetShop(String visitorName) throws ResponseException {
+        try {
+            var action = new Action(Action.Type.EXIT, visitorName);
+            this.session.getBasicRemote().sendText(new Gson().toJson(action));
+        } catch (IOException ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+    }
+
 }
